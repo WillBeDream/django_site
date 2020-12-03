83a139f1fc9084201562cbeb7f50eeff390fed55
Dmitriy <dvol72@mail.ru>
2020-12-03 21:13:50 +0300

    add files

---
+++ django_site/users/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
+from PIL import Image 
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, verbose_name = "Логин" ,on_delete = models.CASCADE)
+    avatar = models.ImageField("Аватар", default = "avatar.png", upload_to = "avatars/")
+
+    def __str__(self):
+        return f"{self.user.username}"
+
+    def save(self):
+        super().save()
+
+        image = Image.open(self.avatar.path)
+
+        if image.height > 250 or image.width > 250:
+            resize = (250,250)
+            image.thumbnail(resize)
+            image.save(self.avatar.path)
+
+
+
+    class Meta:
+        verbose_name = "Профиль"
+        verbose_name_plural = "Профили"
+
+
+
+
+
